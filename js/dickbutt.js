cf5a111a560325fb8003f790435363a69923c2c3
Saurabh KT <saurabh.kt@upgrad.com>
2017-03-12 20:43:47 +0530

    Added smooth scroll, Panda, work links for Sanjana and myself, unused scrollify.js

---
+++ js/dickbutt.js
@@ -0,0 +1,54 @@
+			console.log(`
+                                      Tangestic
+                                  lesalldayerrdayni
+                              ggaTangesticlesalldayerr
+                      dayniggaTangest           iclesall
+                   dayerrdaynigg                  aTanges
+                 ticlesalldayerr                   daynig
+                 gaTangesticlesal                   ldaye
+                 rrdayniggaT angest    iclesallday  errda
+                 yniggaTangesticlesa lldayerrdaynigg aTan
+                 gesticlesalldayer  rdayniggaTangesticles
+                allda  yerrdaynigg  aTangesticlesalldayer
+               rdayniggaTangesticle salldayerrd ayniggaTa
+              ngesticlesalldayerr   dayniggaTangesticlesa
+             lldayerrdayniggaTangesticlesalldayer  rdayn
+            iggaT          angesticlesalldaye     rrdayn
+           iggaT                      angesti     clesal
+          ldayer                                 rdayni
+         ggaTan                                 gestic
+        lesall                                  dayerr
+        dayni                      ggaT        angest
+        icle                      salld aye   rrdayn
+        igga                      Tangesticl  esall                         dayerrday
+       nigga                      Tangestic  lesal                        ldayerrdayni
+       ggaTa                     ngesticles allda                       yerrda    ynig
+       gaTan                     gesticles  allda                     yerrday    nigga
+       Tange                    sticlesal  ldayer                   rdaynig     gaTan
+       gesti                    clesalld   ayerrdayniggaTangest   iclesal     ldaye
+        rrda                   yniggaTa    ngesticlesalldayerrdayniggaT      anges
+        ticl                   esallda     yerrd   aynig   gaTangesti      clesal
+        lday                  errdayni      gga   Tangesticlesallda      yerrda
+        ynigg               aTang estic         lesalldayerrdaynig     gaTange
+         stic             lesal  ldayerr         dayniggaTangesticle   salldaye
+         rrday            niggaTangestic                     lesallda    yerrdayni
+          ggaTa            ngesticlesal              lday       errday  nigg aTang
+          esticl              esal                   lday        errday  niggaTan
+           gestic                                les              allda    yerr
+            daynigga                            Tang              estic     lesa
+               lldayer                          rday              niggaTangestic
+     les        alldayerrd                       ayni           ggaTangesticles
+    alldaye    rrdayniggaTangest                  icl         esallda    y
+    errdayniggaTan gesticlesalldayerrday           nigg    aTanges
+    ticl esalldayerrday    niggaTangesticl esalldayerrdayniggaTa
+     nges  ticlesalld         ayerrdaynig gaTangesticlesallda
+      yerr   daynig         gaTangesticl esall dayerrdayni
+       ggaTangest           iclesallday  errd
+        aynigga              Tangesti   cles
+          all                dayerr    dayn
+                              iggaTa  nges
+                               ticlesalld
+                                 ayerrda
+                                   yni
+                                                                                        
+`);
